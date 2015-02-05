04290ee6243017b8a468eecddf201d7d5070671d
Takahiro Naka <naka.takahiro@gmail.com>
2015-02-06 01:08:39 +0900

    Background fetchを有効にした

--- Treasure/AppDelegate.m
+++ Treasure/AppDelegate.m
@@ -12,10 +12,26 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
-    return YES;
+  //iBeacon信号を検知したときに発火
+  if([launchOptions objectForKey:UIApplicationLaunchOptionsLocationKey]!=nil)
+  {
+    //10 秒後に通知を送るための設定
+    UILocalNotification *notification = [[UILocalNotification alloc] init];
+    notification.fireDate = [[NSDate date] dateByAddingTimeInterval:10];
+    notification.timeZone = [NSTimeZone systemTimeZone];
+    notification.alertBody = @"Notification_location";
+    notification.alertAction = @"Open";
+    
+    // 通知を登録する
+    [application scheduleLocalNotification:notification];
+  }
+
+  
+  [application setMinimumBackgroundFetchInterval:UIApplicationBackgroundFetchIntervalMinimum];
+  
+  return YES;
 }
-							
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
   // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -24,7 +40,7 @@
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-  // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+  // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
   // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
@@ -41,6 +57,14 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
   // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+//Background
+- (void)application:(UIApplication *)application performFetchWithCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
+{
+  NSLog(@"Back Ground Fetch");
+  
+  completionHandler(UIBackgroundFetchResultNewData);//完了
 }
 
 @end
